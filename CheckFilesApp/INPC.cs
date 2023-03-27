33a67f6837f9f73d9ebd5bc6b541fb270b10744f
Sergey Evseev <sevseev@gmail.com>
2023-03-27 18:30:00 +0300

    LoadForbiddenWords

--- CheckFilesApp/INPC.cs
+++ CheckFilesApp/INPC.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CheckFilesApp
 {
-    internal class INPC
+    public class INPC : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string property = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
     }
 }
--- CheckFilesApp/MainWindowViewModel.cs
+++ CheckFilesApp/MainWindowViewModel.cs
@@ -4,9 +4,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.IO;
+using System.ComponentModel;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+
+
 namespace CheckFilesApp
 {
-    internal class MainWindowViewModel
+    internal class MainWindowViewModel : INPC
     {
+        //Выбранная директория
+        private string _selectedDirectory;
+
+        public string SelectedDirectory
+        {
+            get { return _selectedDirectory; }
+
+            set {
+                _selectedDirectory = value;
+                OnPropertyChanged();
+            }
+        }
+        //Список запрещенных слов
+        List <string> _forbiddenWords;
+
+        public List <string> ForbiddenWords
+        {
+            get { return _forbiddenWords;}
+        }
+
+        //метод который загружает список запрещенных слов
+        public bool LoadForbiddenWords(string path="")
+        {
+            //TODO: Добавить проверку файла
+            _forbiddenWords = File.ReadLines(path).ToList();
+            OnPropertyChanged(nameof(ForbiddenWords));
+            return true;
+        }
+        //метод сканирования директории
+        public bool ScanDirectory()
+        {
+            List<string> files = 
+            Directory.GetFiles(_selectedDirectory, "*.txt", SearchOption.AllDirectories).ToList();
+            return true;
+        }
     }
 }
