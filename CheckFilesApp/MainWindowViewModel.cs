48c338a977c53c655264f59445d4c166e3cd3a3a
Sergey Evseev <sevseev@gmail.com>
2023-03-29 16:38:19 +0300

    ScanDirectory() finished

--- CheckFilesApp/MainWindowViewModel.cs
+++ CheckFilesApp/MainWindowViewModel.cs
@@ -14,7 +14,8 @@ namespace CheckFilesApp
 {
     internal class MainWindowViewModel : INPC
     {
-        const string FORB_FILE = "ForbiddenWords.txt";
+        const string FORB_FILE = "ForbiddenWords.txt";//константа с именем файла с запрещ. словами
+        const string COPY_DIRECT_NAME = "Forbidden Files";//дир.для сохр. найденных файлов с запрещ. словами
         
         //Выбранная директория
         private string _selectedDirectory;
@@ -29,12 +30,8 @@ namespace CheckFilesApp
             }
         }
         //Список запрещенных слов
-        List <string> _forbiddenWords;
-
-        public List <string> ForbiddenWords
-        {
-            get { return _forbiddenWords;}
-        }
+        public ObservableCollection <string> ForbiddenWords {get; set;}
+        
 
         //прогресс чтения и перезаписи слов
         private int _progress = 0;
@@ -46,8 +43,8 @@ namespace CheckFilesApp
         //метод который загружает запрещенные слова из файла
         public bool LoadForbiddenWords()
         {
-            //TODO: Добавить проверку файла
-            _forbiddenWords = File.ReadLines(FORB_FILE).ToList();
+        //TODO: Добавить проверку файла
+        ForbiddenWords = new ObservableCollection<string>(File.ReadLines(FORB_FILE).ToList());
             OnPropertyChanged(nameof(ForbiddenWords));
             return true;
         }
@@ -56,7 +53,7 @@ namespace CheckFilesApp
         {
             try
             {                
-                File.WriteAllLines(FORB_FILE, _forbiddenWords);
+                File.WriteAllLines(FORB_FILE, ForbiddenWords);
                 return true;
             }
             catch (Exception ex)
@@ -67,12 +64,33 @@ namespace CheckFilesApp
         }
 
 
-        //метод сканирования директории - найти все текстовые файлы
-        public bool ScanDirectory()
+        //метод сканирования директории - найти все текстовые файлы в директории 
+        public async Task ScanDirectory()
         {
-            List<string> files = //просканировать директорию, привести список файлов к строке и присвоить списку
-            Directory.GetFiles(_selectedDirectory, "*.txt", SearchOption.AllDirectories).ToList();
-            return true;
+            ////просканировать директорию, привести список файлов к строке и сделать список файлов
+            List<string> files = Directory.GetFiles(_selectedDirectory, "*.txt", SearchOption.AllDirectories).ToList();
+            
+            //чтение каждого файла из списка
+            foreach (var file in files)
+            {
+                using (StreamReader stream = new StreamReader(file))
+                {
+                    string line;
+                    while ( (line = await stream.ReadLineAsync()) != null)
+                    {
+                        bool find = false;
+                        foreach (var word in ForbiddenWords) //ObservableCollection
+                          if (line.ToLower().Contains(word.ToLower()))
+                          {
+                                find = true;
+                                var fi = new FileInfo(file); //получаем информацию о файле для имени файла
+                                File.Copy(file, Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+                                    + $"\\{COPY_DIRECT_NAME}\\{fi.Name}_forbidden");
+                                break; 
+                          }
+                    }
+                }
+            }
         }
     }
 }
